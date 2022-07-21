7f87c679e0faa719cacdc60f4cc6c1793aa0fdfb
BorisovaMariia <102960379+BorisovaMariia@users.noreply.github.com>
2022-07-21 11:17:18 +0300

    copy files and create folders by the list

--- copy_files_in_new_folders.py
+++ copy_files_in_new_folders.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from tkinter import Tk, filedialog
 
-root = Tk() # pointing root to Tk() to use it as Tk() in program.
+root = Tk()
 root.withdraw() # Hides small tkinter window.
 
 root.attributes('-topmost', True) # Opened windows will be active. above all windows despite of selection.
